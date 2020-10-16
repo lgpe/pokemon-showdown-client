342e4b436743322a833e4f7e8fca8eb2529da1e6
Eza <tommomuffett@gmail.com>
2020-10-16 21:01:44 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (4ca31918)";
+Config.version = "0.11.2";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
